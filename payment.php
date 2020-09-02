50d55a0bae3125fb5a8be8f167107165bf265371
“pulin” <luching0823@gmail.com>
2020-09-02 23:09:09 +0800

    - google 串接 js驗證

---
+++ payment.php
@@ -0,0 +1,6 @@
+<?php
+        $firstPayment = $_GET["firstPayment"];
+        $secondPayment = $_GET["secondPayment"];
+        $thirdPayment = $_GET["thirdPayment"];
+        header("Location: googlemap.html");
+?>
